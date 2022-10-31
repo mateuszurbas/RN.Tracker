8e7d7f8c9a18296ff10d71f5767406e696c0eca7
Mateusz Urbas <mateusz.urbas@siepomaga.pl>
2022-10-31 22:02:13 +0100

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -3,42 +3,12 @@ import { StyleSheet, View } from "react-native";
 import { SafeAreaProvider, initialWindowMetrics } from "react-native-safe-area-context";
 import { RootStore, RootStoreModel, RootStoreProvider } from "@models/root-store";
 import { DashboardScreen } from "@screens/dashboard";
-import { Tracker, TrackerProject } from "@ts/tracker";
+import { Tracker } from "@ts/tracker";
 import { renderCond } from "@utils/rendering";
 import { storageGetItem, StorageKey } from "@utils/storage";
+import { mockTrackers } from "./app.mock";
 import { color } from "./theme/color";
 
-const mockTrackers: Tracker[] = [
-  {
-    id: "test-id-1",
-    name: "Test",
-    project: TrackerProject.Mango,
-    duration: 0,
-    startDate: new Date(),
-    startActiveDate: new Date(),
-  },
-  {
-    id: "test-id-2",
-    name: "Test",
-    project: TrackerProject.Mango,
-    duration: 32300,
-    startDate: new Date(),
-  },
-  {
-    id: "test-id-3",
-    name: "Test 1",
-    project: TrackerProject.KiwiAndCo,
-    duration: 12300,
-    startDate: new Date(new Date().setDate(new Date().getDate() - 1)),
-  },
-  {
-    id: "test-id-4",
-    name: "Test 2",
-    project: TrackerProject.UXReview,
-    duration: 20123123,
-    startDate: new Date(new Date().setDate(new Date().getDate() - 1)),
-  },
-];
 const App: FC = () => {
   const [rootStore, setRootStore] = useState<RootStore | undefined>(undefined);
   const [isReady, setIsReady] = useState(false);
@@ -53,9 +23,9 @@ const App: FC = () => {
   };
 
   const setupStore = async () => {
-    // TODO: MOCK DATA - on release should be empty array
+    const initTrackerList = __DEV__ ? mockTrackers : [];
     const restoredTrackerList: Tracker[] =
-      (await storageGetItem(StorageKey.TrackerData)) || mockTrackers;
+      (await storageGetItem(StorageKey.TrackerData)) || initTrackerList;
 
     const parsedTrackerList = restoredTrackerList.map((tracker) => ({
       ...tracker,
